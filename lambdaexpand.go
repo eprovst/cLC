a58dbb390e5df6c519b1d0d52482f91104e22ed9
Evert Provoost <evert.provoost@gmail.com>
2017-12-02 23:51:06 +0100

    Heavily improved expansion

--- lambdaexpand.go
+++ lambdaexpand.go
@@ -2,54 +2,14 @@ package LamCalc
 
 import "reflect"
 
-// TODO: Simplfy code, oh and probably doesn't work...
-
 // Substitute replaces index by sub
 func (lf LamFunc) Substitute(index int, sub LamTerm) LamTerm {
-	nw := LamFunc{}
-	for _, term := range lf {
-		switch term := term.(type) {
-		case int:
-			if term == index {
-				nw = append(nw, sub)
-			} else {
-				nw = append(nw, term)
-			}
-
-		case LamExpr:
-			nw = append(nw, term.Substitute(index, sub))
-
-		case LamFunc:
-			nw = append(nw, term.Substitute(index+1, sub))
-
-		}
-	}
-
-	return nw
+	return LamFunc(LamExpr(lf).Substitute(index, sub).(LamExpr))
 }
 
 // Insert places sub into the function whilst consuming the latter
 func (lf LamFunc) Insert(sub LamTerm) LamTerm {
-	nw := LamExpr{}
-	for _, term := range lf {
-		switch term := term.(type) {
-		case int:
-			if term == 0 {
-				nw = append(nw, sub)
-			} else {
-				nw = append(nw, term)
-			}
-
-		case LamExpr:
-			nw = append(nw, term.Substitute(0, sub))
-
-		case LamFunc:
-			nw = append(nw, term.Substitute(1, sub))
-
-		}
-	}
-
-	return nw
+	return LamExpr(lf).Substitute(0, sub)
 }
 
 // ExpandOnce expands a lambda function (which means doing nothing)
@@ -62,7 +22,7 @@ func (lf LamFunc) Expand() LamFunc {
 	return lf
 }
 
-// Substitute fills relpaces index by sub
+// Substitute fills replaces index by sub
 func (lx LamExpr) Substitute(index int, sub LamTerm) LamTerm {
 	nw := LamExpr{}
 	for _, term := range lx {
@@ -70,6 +30,7 @@ func (lx LamExpr) Substitute(index int, sub LamTerm) LamTerm {
 		case int:
 			if term == index {
 				nw = append(nw, sub)
+
 			} else {
 				nw = append(nw, term)
 			}
@@ -83,10 +44,6 @@ func (lx LamExpr) Substitute(index int, sub LamTerm) LamTerm {
 		}
 	}
 
-	if len(nw) == 1 {
-		return nw[0].(LamTerm)
-	}
-
 	return nw
 }
 
@@ -94,30 +51,33 @@ func (lx LamExpr) Substitute(index int, sub LamTerm) LamTerm {
 func (lx LamExpr) ExpandOnce() LamTerm {
 	nw := LamExpr{}
 
-	switch term := lx[0].(type) {
-	case LamExpr:
-		nw = append(nw, term.Expand())
-		if len(lx) > 1 {
-			nw = append(nw, lx[1:]...)
-		}
+	if reflect.TypeOf(lx[0]).String() == "LamCalc.LamFunc" {
+		if len(lx) < 2 {
+			return lx[0].(LamFunc)
 
-		return nw
+		} else if reflect.TypeOf(lx[0]).String() == "LamCalc.LamFunc" || reflect.TypeOf(lx[0]).String() == "LamCalc.LamFunc" {
+			nw = append(nw, lx[0].(LamFunc).Insert(lx[1].(LamTerm)))
 
-	case LamFunc:
-		if len(lx) > 1 {
-			nw = append(nw, term.Insert(lx[1].(LamTerm)))
 			if len(lx) > 2 {
-				return append(nw, lx[2:]...)
+				nw = append(nw, lx[2:]...)
 			}
 
-			return nw[0].(LamExpr)
+			return nw.ExpandOnce()
 		}
 
-		return term
+	} else {
+		for _, term := range lx {
+			switch term := term.(type) {
+			case int:
+				nw = append(nw, term)
 
-	default:
-		return lx
+			case LamTerm:
+				nw = append(nw, term.ExpandOnce())
+			}
+		}
 	}
+
+	return nw
 }
 
 // Expand expands a lambda expression
