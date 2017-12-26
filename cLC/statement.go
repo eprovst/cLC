c0745dcf6fe4520cdc94e6d7ccc14158045a077b
Evert Provoost <evert.provoost@gmail.com>
2017-12-26 20:43:04 +0100

    Hotfix clear on Windows

--- cLC/statement.go
+++ cLC/statement.go
@@ -29,7 +29,7 @@ func executeStatement(stmnt cLCStatement) {
 		var clearCmd *exec.Cmd
 
 		if runtime.GOOS == "windows" {
-			clearCmd = exec.Command("cls")
+			clearCmd = exec.Command("cmd", "/c", "cls")
 		} else {
 			clearCmd = exec.Command("clear")
 		}
