ce7ff72170d5135e432751f577c47ffa4bde6149
Evert Provoost <evert.provoost@gmail.com>
2018-05-25 14:10:17 +0200

    Reduce size of builds

--- cLC/build.sh
+++ cLC/build.sh
@@ -3,12 +3,12 @@
 rm -rf build
 
 # Build for current platform
-go build -o build/current/cLC
+go build -o build/current/cLC -ldflags "-s -w"
 
 # Make sure Windows users get an .exe
 cp build/current/cLC build/current/cLC.exe
 
 # Build for 64bit Linux and Windows
-GOARCH=amd64 GOOS=linux go build -o build/linux/cLC
+GOARCH=amd64 GOOS=linux go build -o build/linux/cLC -ldflags "-s -w"
 
-GOARCH=amd64 GOOS=windows go build -o build/windows/cLC.exe
+GOARCH=amd64 GOOS=windows go build -o build/windows/cLC.exe -ldflags "-s -w"
